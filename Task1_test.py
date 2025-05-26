450b09c4038f30b59b6759a72789b82a44544534
BilalM07 <68804458+BilalM07@users.noreply.github.com>
2025-05-26 14:02:19 +0530

    Update Task1_test.py

--- Task1_test.py
+++ Task1_test.py
@@ -32,12 +32,10 @@ def test_clean_albums_df_removes_unneeded_columns():
         'artist_11': [None]
     })
     cleaned_df = clean_albums_df(sample_df.copy())
-    assert 'track_name' in cleaned_df.columns  
-    assert 'release_date' in cleaned_df.columns
-    assert 'label' in cleaned_df.columns
-    assert 'track_id' in cleaned_df.columns
-    assert 'duration_ms' in cleaned_df.columns
+    for i in [ 'track_name' , 'release_date', 'label', 'track_id', 'duration_ms']:
+        assert i in cleaned_df.columns  
 # all important columns should remain after cleaning.
+    print('Testing complete, clean_albums_df module working as expected.') # This line is for confirmation, in case of failiure asssertions will raise errors.
 
 def test_transform_and_merge_filters_correctly():
     tracks_df = pd.DataFrame({
@@ -50,18 +48,17 @@ def test_transform_and_merge_filters_correctly():
         'release_date':['015-06-09 00:00:00 UTC'],
         'label':['Test Label'],
         'track_id': ['1'],
-        'duration_sec': [180],
         'duration_ms': [180000]
     })
     result = transform_and_merge(tracks_df.copy(), albums_df.copy())
     assert len(result) == 1  # Only one track should pass the filters
     assert result.iloc[0]['radio_mix'] == True # Radio mix is True for duration <= 3 minutes
     assert result.iloc[0]['explicit'] == False # Explicit should be False
+    print('Testing complete, transform_and_merge module working as expected.') # This line is for confirmation, in case of failiure asssertions will raise errors.
 
 def main():
     test_clean_albums_df_removes_unneeded_columns()
     test_transform_and_merge_filters_correctly()
-    print('Testing complete. Modules working as expected.') # This line is for confirmation, in case of failiure asssertions will raise errors.
 
 if __name__ == "__main__":
     main()
